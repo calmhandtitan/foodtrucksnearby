db7ed7823cf8d541a83c359e3d5fba72431339d6
Chandan Mittal <chandan.iiitdmj@gmail.com>
2016-07-26 04:51:16 +0530

    update

--- foodtrucknearby/settings.py
+++ foodtrucknearby/settings.py
@@ -107,5 +107,6 @@ USE_TZ = True
 STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'foodtrucks/static'),
 )
+print STATICFILES_DIRS

fc1cae67d3bc9ad742927bcce4adfa2ad06b2f6b
Chandan Mittal <chandan.iiitdmj@gmail.com>
2016-08-01 11:11:19 +0530

    Revert "addded pep8 code styling using autopep8"
    This reverts commit b0ac281e324192c66e07d72fbef0178884fda075.
    
    Conflicts:
    	foodtrucks/forms.py

--- foodtrucks/forms.py
+++ foodtrucks/forms.py
@@ -3,8 +3,20 @@ import decimal
 
 
 class QueryForm(forms.Form):
+<<<<<<< HEAD
     latitude = forms.DecimalField(widget=forms.HiddenInput())
     longitude = forms.DecimalField(widget=forms.HiddenInput())
+=======
+	latitude = forms.DecimalField(widget = forms.HiddenInput())
+	longitude = forms.DecimalField(widget = forms.HiddenInput())
+    
+	radius = forms.DecimalField(max_digits=6, decimal_places=3, min_value=decimal.Decimal(0), initial=1.0, \
+	                            widget = forms.NumberInput(attrs={'class':'form-control',\
+	                                'placeholder':'1.0', 'autofocus':'on'}))
+	limit = forms.IntegerField(min_value=0, initial=10, \
+								widget = forms.NumberInput(attrs={'class':'form-control',\
+	                                'placeholder':'10', 'autofocus':'on'}))
+>>>>>>> parent of b0ac281... addded pep8 code styling using autopep8
 
     radius = forms.DecimalField(max_digits=6, decimal_places=3, min_value=decimal.Decimal(0), initial=1.0,
                                 widget=forms.NumberInput(attrs={'class': 'form-control',
